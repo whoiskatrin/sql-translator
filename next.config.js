5e4dc0cfa90b3c2d9b94bb8c29791a93c57d4b49
“whoiskatrin” <katjareznikova@gmail.com>
2023-03-11 22:53:22 +0000

    update configs

--- next.config.js
+++ next.config.js
@@ -2,5 +2,5 @@
 module.exports = {
   reactStrictMode: true,
   env: {
-    OPENAI_API_KEY: "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"},
+    OPENAI_API_KEY: "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"},
 };
