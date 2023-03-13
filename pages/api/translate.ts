e25942436e5188b57400f53bfe7cab53affdd6a7
sadmann7 <sadmann898.gmail.com>
2023-03-14 00:41:59 +0600

    Update ChatGPTModel types, and no environment variable logics

--- pages/api/translate.ts
+++ pages/api/translate.ts
@@ -1,11 +1,24 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import translateToSQL from "../../src/translateToSQL";
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+if (!process.env.OPENAI_API_KEY) {
+  throw new Error(
+    "OPENAI_API_KEY is not defined in .env file. Please add it there (see README.md for more details)."
+  );
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   const { inputText, tableSchema } = req.body;
   try {
-    const result = await translateToSQL(inputText, process.env.OPENAI_API_KEY, tableSchema);
-    console.log(res)
+    const result = await translateToSQL(
+      inputText,
+      process.env.OPENAI_API_KEY,
+      tableSchema
+    );
+    console.log(res);
     res.status(200).json({ outputText: result });
   } catch (error) {
     console.error(error);
