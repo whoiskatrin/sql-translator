bf5bf1c0e34cdba62398a02bed240bd8dd921b4c
sadmann7 <sadmann898@gmail.com>
2023-03-19 10:33:11 +0600

    disable copy button on transitioning

--- pages/index.tsx
+++ pages/index.tsx
@@ -305,8 +305,9 @@ export default function Home() {
 
             <div className="flex items-center mt-5">
               <button
-                className="flex items-center justify-center space-x-4 rounded-full border bg-gradient-to-r from-gray-50 to-gray-100 text-black px-5 py-2 text-sm hover:bg-blue-500 bg-blue-600 font-medium transition"
+                className="flex items-center disabled:pointer-events-none disabled:opacity-70 justify-center space-x-4 rounded-full border bg-gradient-to-r from-gray-50 to-gray-100 text-black px-5 py-2 text-sm hover:bg-blue-500 bg-blue-600 font-medium transition"
                 onClick={handleCopy}
+                disabled={outputText.length === 0 || isCopied}
               >
                 <img src="/copyDark.svg" alt="Copy" />
               </button>
