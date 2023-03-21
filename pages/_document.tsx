d7c9c06ccc08b89cdde5ac47bfd131aafe7a4b90
Aaroh Mankad <amankad@plaid.com>
2023-03-21 09:53:20 -0700

    remove vertical scroll with p-4 => px-4

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -24,7 +24,7 @@ class MyDocument extends Document {
           />
         </Head>
         <body className="dark:bg-black bg-gray-100 text-gray-800 dark:text-white font-sans">
-          <div className="max-w-4xl mx-auto p-4">
+          <div className="max-w-4xl mx-auto px-4">
             <Main />
           </div>
           <NextScript />
