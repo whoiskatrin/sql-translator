b7e57d87c0e4aac47ff1a5db060eef2d1071080e
jack_kwok <guow.weizhan@gmail.com>
2023-04-03 13:52:39 +0800

    update test funtcion to async

--- test/testSingleQuery.ts
+++ test/testSingleQuery.ts
@@ -11,41 +11,41 @@ export function run () {
         + "CREATE TABLE customer (cus_id INT, name TEXT, sex INT, age INT);"
         + "CREATE TABLE order (order_id INT, car_id INT, cus_id INT, number INT, price DOUBLE, date DATE);";
        
-    test('show me all the cars that are red', t => {
+    test('show me all the cars that are red',async t => {
         const inputText = "show me all the cars that are red";
         const sql = "SELECT * FROM cars WHERE color = 'red'";
         const case1: TestCase = { inputText, sql};
         const result = translateToSQL(case1.inputText, process.env.OPENAI_API_KEY, tableSchema);
-        t.deepEqual(result, case1.sql);
+        t.deepEqual(await result, case1.sql);
     });
-    test('show me the count of cars', t => {
+    test('show me the count of cars',async t => {
         const inputText = "show me the count of cars";
         const sql = "SELECT COUNT() FROM cars";
         const case1: TestCase = {inputText, sql};
         const result = translateToSQL(case1.inputText, process.env.OPENAI_API_KEY, tableSchema);
-        t.deepEqual(result, case1.sql);
+        t.deepEqual(await result, case1.sql);
     });
-    test('show me the count of cars that are green', t => {
+    test('show me the count of cars that are green',async t => {
         const inputText = "show me the count of cars that are green";
         const sql = "SELECT COUNT() FROM cars WHERE color = 'green'";
         const case1: TestCase = {inputText, sql};
         const result = translateToSQL(case1.inputText, process.env.OPENAI_API_KEY, tableSchema);
-        t.deepEqual(result, case1.sql);
+        t.deepEqual(await result, case1.sql);
     });
-    test('show me the customer that age older than 20 years', t => {
+    test('show me the customer that age older than 20 years',async t => {
         const inputText = "show me the customer that age older than 20 years";
         const sql = "SELECT * FROM customer WHERE age>20";
         const case1: TestCase = {inputText, sql};
         const result = translateToSQL(case1.inputText, process.env.OPENAI_API_KEY, tableSchema);
-        t.deepEqual(result, case1.sql);
+        t.deepEqual(await result, case1.sql);
     });
-    test('show me the total order count that date between \'2023-03-01\' AND \'2023-03-31\'', t => {
+    test('show me the total order count that date between \'2023-03-01\' AND \'2023-03-31\'',async t => {
         const inputText = "show me the total order count that date between \'2023-03-01\' AND \'2023-03-31\'";
         const sql = "SELECT sum(count) FROM order WHERE date BETWEEN '2023-03-01' AND '2023-03-31'";
         const case1: TestCase = {inputText, sql};
         const result = translateToSQL(case1.inputText, process.env.OPENAI_API_KEY, tableSchema);
-        t.deepEqual(result, case1.sql);
+        t.deepEqual(await result, case1.sql);
     });
-
+    
 }
 
