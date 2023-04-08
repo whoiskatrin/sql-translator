fad4016cc3eee955c9537b42ca7bb90859e60da9
guoweizhan <guoweizhan@xiaomi.com>
2023-04-09 00:31:48 +0800

    remove test example file

--- test/test.ts
+++
@@ -1,9 +0,0 @@
-describe("Math functions", () => {
-    it("should multiply 5 by 3", () => {
-      expect(15).toEqual(15);
-    });
-  
-    it("should add 5 by 3", () => {
-      expect(8).toEqual(8);
-    });
-  });
